6386f77eb1e33cf20ae49830d9dc4a30a01bf19d
kasymov_07 <mistaru@bk.ru>
2020-07-30 13:10:45 +0600

    add button delete entity

--- src/main/webapp/reactjs/src/components/Employee.js
+++ src/main/webapp/reactjs/src/components/Employee.js
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlusSquare, faSave, faUndo} from "@fortawesome/free-solid-svg-icons";
 import axios from 'axios';
 import MyToast from "./MyToast";
+
 export default class Employee extends Component {
     constructor(props) {
         super(props);
@@ -14,7 +15,7 @@ export default class Employee extends Component {
     }
 
     initialState = {
-        name:'', surname:'', email:'', birthday:'', phoneNumber:''
+        name: '', surname: '', email: '', birthday: '', phoneNumber: ''
     }
 
     resetEmployee = () => {
@@ -24,7 +25,7 @@ export default class Employee extends Component {
     submitEmployee = event => {
         event.preventDefault()
 
-        const  employee = {
+        const employee = {
             name: this.state.name,
             surname: this.state.surname,
             email: this.state.email,
@@ -34,22 +35,21 @@ export default class Employee extends Component {
 
         axios.post("http://localhost:9090/employee", employee)
             .then(response => {
-            if (response.data != null) {
-                this.setState({"show": true});
-                setTimeout(() => this.setState({"show":false}),2000);
-            } else {
-                this.setState({"show": false});
-
-            }
-        });
+                if (response.data != null) {
+                    this.setState({"show": true});
+                    setTimeout(() => this.setState({"show": false}), 2000);
+                } else {
+                    this.setState({"show": false});
+                }
+            });
 
         this.setState(this.initialState);
-
     }
+
 
     EmployeeChange = event => {
         this.setState({
-        [event.target.name]:event.target.value
+            [event.target.name]: event.target.value
         });
     }
 
@@ -58,79 +58,78 @@ export default class Employee extends Component {
 
         return (
             <div>
-                <div style={{"display": this.state.show? "block" : "none"}}>
-                    <MyToast children={{show:this.state.show, message:"Saved Successfully"}}/>
+                <div style={{"display": this.state.show ? "block" : "none"}}>
+                    <MyToast children={{show: this.state.show, message: "Saved Successfully", type: "success"}}/>
                 </div>
 
 
+                <Card className="border border-dark bg-gray text-black">
+                    <Card.Header><FontAwesomeIcon icon={faPlusSquare}/>
+                        Add Employee
+                    </Card.Header>
+                    <Form onReset={this.resetEmployee} onSubmit={this.submitEmployee} id="bookFormId">
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridName">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="name"
+                                                  value={name}
+                                                  onChange={this.EmployeeChange}
+                                                  className={"bg-gray text-black"}
+                                                  placeholder="Enter name"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridSurname">
+                                    <Form.Label>Surname</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="surname"
+                                                  value={surname}
+                                                  onChange={this.EmployeeChange}
+                                                  className={"bg-gray text-black"}
+                                                  placeholder="Enter surname"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="email" name="email"
+                                                  value={email}
+                                                  onChange={this.EmployeeChange}
+                                                  className={"bg-gray text-black"}
+                                                  placeholder="Enter email"/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridBirthday">
+                                    <Form.Label>Birthday</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="date" name="birthday"
+                                                  value={birthday}
+                                                  onChange={this.EmployeeChange}
+                                                  className={"bg-gray text-black"}
+                                                  placeholder="Enter birthday"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridPhoneNumber">
+                                    <Form.Label>Phone number</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="phoneNumber"
+                                                  value={phoneNumber}
+                                                  onChange={this.EmployeeChange}
+                                                  className={"bg-gray text-black"}
+                                                  placeholder="Enter phone number"/>
+                                </Form.Group>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign": "right"}}>
+                            <Button size="sm" variant="success" type="submit">
+                                <FontAwesomeIcon icon={faSave}/> Submit
+                            </Button>{' '}
 
-            <Card className="border border-dark bg-gray text-black">
-                <Card.Header><FontAwesomeIcon icon={faPlusSquare} />
-                    Add Employee
-                </Card.Header>
-                <Form onReset={this.resetEmployee} onSubmit={this.submitEmployee} id="bookFormId">
-                <Card.Body>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId = "formGridName">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control required autoComplete = "off"
-                                    type="text" name="name"
-                                    value={name}
-                                    onChange={this.EmployeeChange}
-                                    className={"bg-gray text-black"}
-                                    placeholder="Enter name" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId = "formGridSurname">
-                                <Form.Label>Surname</Form.Label>
-                                <Form.Control required autoComplete = "off"
-                                    type="text" name="surname"
-                                    value={surname}
-                                    onChange={this.EmployeeChange}
-                                    className={"bg-gray text-black"}
-                                    placeholder="Enter surname" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId = "formGridEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control required autoComplete = "off"
-                                    type="email" name="email"
-                                    value={email}
-                                    onChange={this.EmployeeChange}
-                                    className={"bg-gray text-black"}
-                                    placeholder="Enter email" />
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId = "formGridBirthday">
-                                <Form.Label>Birthday</Form.Label>
-                                <Form.Control required autoComplete = "off"
-                                    type="date" name="birthday"
-                                    value={birthday}
-                                    onChange={this.EmployeeChange}
-                                    className={"bg-gray text-black"}
-                                    placeholder="Enter birthday" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId = "formGridPhoneNumber">
-                                <Form.Label>Phone number</Form.Label>
-                                <Form.Control required autoComplete = "off"
-                                    type="text" name="phoneNumber"
-                                    value={phoneNumber}
-                                    onChange={this.EmployeeChange}
-                                    className={"bg-gray text-black"}
-                                    placeholder="Enter phone number" />
-                            </Form.Group>
-                        </Form.Row>
-                </Card.Body>
-                    <Card.Footer style = {{"textAlign":"right"}}>
-                    <Button size = "sm" variant="success" type="submit">
-                        <FontAwesomeIcon icon={faSave} /> Submit
-                    </Button>{' '}
-
-                    <Button size = "sm" variant="info" type="reset">
-                        <FontAwesomeIcon icon={faUndo} /> Reset
-                    </Button>
-                    </Card.Footer>
-                </Form>
-            </Card>
+                            <Button size="sm" variant="info" type="reset">
+                                <FontAwesomeIcon icon={faUndo}/> Reset
+                            </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
             </div>
         )
     }
--- src/main/webapp/reactjs/src/components/List.js
+++ src/main/webapp/reactjs/src/components/List.js
@@ -3,6 +3,7 @@ import {Card, Table, ButtonGroup, Button} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faList, faTrash} from "@fortawesome/free-solid-svg-icons";
 import axios from 'axios';
+import MyToast from "./MyToast";
 
 export default class List extends Component {
 
@@ -25,55 +26,76 @@ export default class List extends Component {
                 this.setState({employees: data});
             });
 
-    }
+    };
+
+    deleteEmployee = (employeeId) => {
+        axios.delete("http://localhost:9090/employee/" + employeeId)
+            .then(response => {
+                if(response.data != null) {
+                    this.setState({"show": true});
+                    setTimeout(() => this.setState({"show":false}),2000);
+                    this.setState({
+                        employees : this.state.employees.filter(employee => employee.id !== employeeId)
+                    });
+                } else {
+                 this.setState({"show": false});
+        }
+            });
+    };
 
     render() {
         return (
-            <Card className="border border-dark bg-gray text-black">
-                <Card.Header><FontAwesomeIcon icon={faList}/>
-                    List Employee
-                </Card.Header>
-                <Card.Body>
-                    <Table bordered hover striped variant="gray">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>SurName</th>
-                            <th>Birthday</th>
-                            <th>email</th>
-                            <th>Phone Number</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.employees.length === 0 ?
-                                <tr align="center">
-                                    <td colSpan="6"> Employees Available</td>
-                                </tr> :
-                                this.state.employees.map((employee) => (
-                                    <tr key={employee.id}>
-                                        <td>{employee.name}</td>
-                                        <td>{employee.surname}</td>
-                                        <td>{employee.email}</td>
-                                        <td>{employee.birthday}</td>
-                                        <td>{employee.phoneNumber}</td>
-                                        <td>
-                                            <ButtonGroup>
-                                                <Button size="sm" variant="outline-primary"><FontAwesomeIcon
-                                                    icon={faEdit}/></Button>{' '}
-                                                <Button size="sm" variant="outline-danger"><FontAwesomeIcon
-                                                    icon={faTrash}/></Button>{' '}
-                                            </ButtonGroup>
-                                        </td>
-                                    </tr>
-                                ))
-                        }
+            <div>
+                <div style={{"display": this.state.show? "block" : "none"}}>
+                    <MyToast children={{show:this.state.show, message:"Deleted Successfully", type:"danger"}}/>
+                </div>
+                <Card className="border border-dark bg-gray text-black">
+                    <Card.Header><FontAwesomeIcon icon={faList}/>
+                        List Employee
+                    </Card.Header>
+                    <Card.Body>
+                        <Table bordered hover striped variant="gray">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>SurName</th>
+                                <th>Birthday</th>
+                                <th>email</th>
+                                <th>Phone Number</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.employees.length === 0 ?
+                                    <tr align="center">
+                                        <td colSpan="6"> Employees Available</td>
+                                    </tr> :
+                                    this.state.employees.map((employee) => (
+                                        <tr key={employee.id}>
+                                            <td>{employee.name}</td>
+                                            <td>{employee.surname}</td>
+                                            <td>{employee.email}</td>
+                                            <td>{employee.birthday}</td>
+                                            <td>{employee.phoneNumber}</td>
+                                            <td>
+                                                <ButtonGroup>
+                                                    <Button size="sm" variant="outline-primary"><FontAwesomeIcon
+                                                        icon={faEdit}/></Button>{' '}
+                                                    <Button size="sm" variant="outline-danger" onClick={this.deleteEmployee.bind(this, employee.id)}><FontAwesomeIcon
+                                                        icon={faTrash}/></Button>{' '}
+                                                </ButtonGroup>
+                                            </td>
+                                        </tr>
+                                    ))
+                            }
 
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
+            </div>
+
         )
     }
 }
